6b5f0baa5250c5f363cf44aacef95d0fba7a501e
Ryan Bahan <ryan@ryanbahan.com>
2020-06-12 12:26:00 -0600

    Use next-pwa for service worker; generate manifest.json; add meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ import NextDocument from 'next/document'
 import { Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet as StyledComponentSheets } from 'styled-components'
 import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/core/styles'
+
 export default class Document extends NextDocument {
   static async getInitialProps (ctx) {
     const styledComponentSheet = new StyledComponentSheets()
@@ -38,6 +39,17 @@ export default class Document extends NextDocument {
       <html lang="en">
         <Head>
           {this.props.styleTags}
+          <meta charset='utf-8' />
+          <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+          <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
+          <meta name='description' content='Description' />
+          <meta name='keywords' content='Keywords' />
+
+          <link rel="manifest" href="/manifest.json" />
+          <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
+          <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
+          <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+          <meta name="theme-color" content="#317EFB" />
           <script src="/pts.min.js"></script>
           <script src="/noAutoScroll.js"></script>
           <link href="https://fonts.googleapis.com/css?family=Spartan:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet" />
